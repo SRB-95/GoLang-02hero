5bb8879857b6e60bdc873c936d5147a165218fe0
SRB-95 <maaps.university@gmail.com>
2024-09-30 13:30:51 +0530

    codeCleanup

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"golang-02hero/commandLineArgs"
 	"golang-02hero/dataTypes"
+	"golang-02hero/functions"
 )
 
 func helloGo() {
@@ -15,9 +17,17 @@ func main() {
 
 	fmt.Println("function: PlayWithDataTypes")
 	dataTypes.PlayWithDataTypes()
-	fmt.Println("-----------------------------------: Data Types :------------------------------------>")
+	fmt.Println("---------------------------------: Data Types :------------------------------------>")
+
+	fmt.Println("function: PlayWithFunctions")
+	functions.PlayWithFunctions()
+	fmt.Println("-----------------------------------: Functions :----------------------------------->")
+
+	fmt.Println("function: PlayWithCommandLineArgs")
+	commandLineArgs.PlayWithCommandLineArgs()
+	fmt.Println("-------------------------------: Command Line Args :------------------------------->")
 
 	fmt.Println("function: playWithAssignments")
-	// playWithAssignments()
-	fmt.Println("----------------------------------------------------")
+	playWithAssignments()
+	fmt.Println("----------------------------------: Assignmants :---------------------------------->")
 }
